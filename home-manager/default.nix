4aa773b27e973c703157819acb22c83e5de3ea52
Elliot Berman <eberman@anduril.com>
2026-02-27 19:34:57 -0800

    move nbr to a dedicated script file

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -98,28 +98,10 @@
       (writeShellApplication {
         # Build on any remote machine which is ssh-able.
         name = "nbr";
-        text = ''
-          (( $# != 2 )) && { echo "expected two arguments (got $#): nbr <flakeURI> <remote>"; return 1; }
-          flakeURI=$1
-          remote=$2
-          nix=nom
-
-          if command -v $nix >/dev/null 2>&1 ; then
-            nix=nix
-          fi
-
-          echo "Evaluating $flakeURI..."
-          drvPath=$(nix eval --raw "$flakeURI.drvPath")
-          echo "Instantiated $drvPath."
-
-          echo "Copying $drvPath to $remote..."
-          nix copy "$drvPath" --to "ssh-ng://$remote"
-          echo "Copied $drvPath to $remote."
-
-          echo "Building $drvPath^* on $remote..."
-          nix build -L "$drvPath^*" --store "ssh-ng://$remote" --builders "ssh-ng://$remote" --keep-going --print-out-paths
-          echo "Built $flakeURI on $remote."
-        '';
+        runtimeInputs = [
+          nix-output-monitor
+        ];
+        text = lib.fileContents ./scripts/nbr.bash;
       })
 
       (writeShellApplication {

ef605dd9dc184b945ce04ecb89f98e79c215efc5
Elliot Berman <eberman@anduril.com>
2026-04-02 17:44:25 -0700

    mail updates

--- home-manager/mail.nix
+++ home-manager/mail.nix
@@ -1,58 +1,55 @@
 { config, lib, ... }:
+let
+  inherit (config.accounts.email.accounts.elliotjb) enable;
+in
 {
-  options.eberman.email.passwordCommand = lib.mkOption {
-    default = null;
-    type = with lib.types; nullOr str;
-  };
-
-  config = lib.mkIf (config.eberman.email.passwordCommand != null) {
-    programs = {
-      mbsync = {
-        enable = true;
-      };
-
-      notmuch = {
-        enable = true;
-      };
-
-      neomutt = {
-        enable = true;
-      };
+  programs = {
+    mbsync = {
+      inherit enable;
     };
 
-    accounts.email.accounts.elliotjb = {
-      address = "elliotjb@elliotjb.com";
-      aliases = [
-        "elliotjb+anduril@elliotjb.com"
-      ];
-      realName = "Elliot Berman";
-      primary = true;
-      inherit (config.eberman.email) passwordCommand;
+    notmuch = {
+      inherit enable;
+    };
 
-      mbsync = {
-        enable = true;
-      };
+    neomutt = {
+      inherit enable;
+    };
+  };
 
-      notmuch = {
-        enable = true;
-      };
+  accounts.email.accounts.elliotjb = {
+    address = "elliotjb@elliotjb.com";
+    aliases = [
+      "elliotjb+anduril@elliotjb.com"
+    ];
+    realName = "Elliot Berman";
+    primary = true;
+    enable = lib.mkDefault false;
 
-      neomutt = {
-        enable = true;
-      };
+    mbsync = {
+      enable = true;
+    };
 
-      userName = "elliotjb@elliotjb.com";
-      imap = {
-        host = "mail.privateemail.com";
-        port = 993;
-        tls.enable = true;
-      };
+    notmuch = {
+      neomutt.enable = true;
+      enable = true;
+    };
 
-      smtp = {
-        host = "mail.privateemail.com";
-        port = 465;
-        tls.enable = true;
-      };
+    neomutt = {
+      enable = true;
+    };
+
+    userName = "elliotjb@elliotjb.com";
+    imap = {
+      host = "mail.privateemail.com";
+      port = 993;
+      tls.enable = true;
+    };
+
+    smtp = {
+      host = "mail.privateemail.com";
+      port = 465;
+      tls.enable = true;
     };
   };
 }
